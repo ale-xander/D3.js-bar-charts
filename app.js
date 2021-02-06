dab88f527ac37c85760c6d8f795981256187fc2d
alexiliev <iliev.alex@outlook.com>
2021-02-05 18:00:57 -0600

    inverting the scales

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ d3.json('menu.json').then(data => {
 
     const y = d3.scaleLinear()
       .domain([0, d3.max(data, d => d.orders)])
-      .range([0, graphHeight]);
+    //   
+    .range([graphHeight, 0]);
   
     // const min = d3.min(data, d=> d.orders)
     // console.log('min '+ min)
@@ -62,22 +63,25 @@ d3.json('menu.json').then(data => {
   
     // add attrs to circs already in the DOM
     rects.attr('width', x.bandwidth)
-      .attr("height", d => y(d.orders))
+    //   .attr("height", d => y(d.orders))
+      .attr("height", d => graphHeight - y(d.orders))
       .attr('fill', 'orange')
       .attr('x', d => x(d.name))
+      .attr('y', d => y(d.orders))
   
     // append the enter selection to the DOM
     rects.enter()
       .append('rect')
         .attr('width', x.bandwidth)
-        .attr("height", d => y(d.orders))
+        // .attr("height", d => y(d.orders))
+        .attr("height", d => graphHeight - y(d.orders))
         .attr('fill', 'orange')
         .attr('x', d => x(d.name))
+        .attr('y', d => y(d.orders))
 
     // create and call the axes
     const xAxis = d3.axisBottom(x) //needs to know where to position stuff on the X axis
     const yAxis = d3.axisLeft(y)
-
     xAxisGroup.call(xAxis);
     yAxisGroup.call(yAxis)
   
