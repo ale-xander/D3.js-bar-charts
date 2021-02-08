50b81379d7d1367e058bc680cbdadf92468384e7
alexiliev <iliev.alex@outlook.com>
2021-02-07 22:11:35 -0600

    replacing the JSON with Firebase

--- app.js
+++ app.js
@@ -30,10 +30,53 @@ const yAxisGroup = graph.append('g')
 
 
 
+db.collection('menu-items').get().then(res =>{
+    // console.log(res)
+    let data = [];
+    res.docs.forEach(doc => {
+        // console.log(doc.data())
+        data.push(doc.data())
+    });
+    // console.log(data)
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.orders)])
+        .range([graphHeight, 0]);
+    
+    const x = d3.scaleBand()
+    .domain(data.map(item => item.name))
+    .range([0, 500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
+    const rects = graph.selectAll('rect') 
+            .data(data);
+    rects.attr('width', x.bandwidth)
+        .attr("height", d => graphHeight - y(d.orders))
+        .attr('fill', 'orange')
+        .attr('x', d => x(d.name))
+        .attr('y', d => y(d.orders))
+    rects.enter()
+        .append('rect')
+        .attr('width', x.bandwidth)
+        // .attr("height", d => y(d.orders))
+        .attr("height", d => graphHeight - y(d.orders))
+        .attr('fill', 'orange')
+        .attr('x', d => x(d.name))
+        .attr('y', d => y(d.orders))
+    const xAxis = d3.axisBottom(x)
+                    
+    const yAxis = d3.axisLeft(y)
+                    .ticks(3)
+                    .tickFormat(d => d + ' orders')
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+    xAxisGroup.selectAll('text')
+            .attr('transform', 'rotate(-40)')
+            .attr('text-anchor', 'end')
+            .attr('fill', 'orange')
+})
 
 
-
-
+/*
 d3.json('menu.json').then(data => {
 
     const y = d3.scaleLinear()
@@ -81,8 +124,16 @@ d3.json('menu.json').then(data => {
 
     // create and call the axes
     const xAxis = d3.axisBottom(x) //needs to know where to position stuff on the X axis
+                    
     const yAxis = d3.axisLeft(y)
+                    .ticks(3)
+                    .tickFormat(d => d + ' orders')
     xAxisGroup.call(xAxis);
-    yAxisGroup.call(yAxis)
+    yAxisGroup.call(yAxis);
+    xAxisGroup.selectAll('text')
+            .attr('transform', 'rotate(-40)')
+            .attr('text-anchor', 'end')
+            .attr('fill', 'orange')
   
   });
+  */
